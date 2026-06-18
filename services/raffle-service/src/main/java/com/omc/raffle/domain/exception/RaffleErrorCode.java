d7cf32d5900c8eb0f61d9d82cb4fe730c954d5e8
jaehoon0321 <jaehoon0321@gmail.com>
2026-06-18 11:07:56 +0900

    fix: develop 브랜치 변경사항 충돌 해결 및 ErrorCode 상속 제거

--- services/raffle-service/src/main/java/com/omc/raffle/domain/exception/RaffleErrorCode.java
+++ services/raffle-service/src/main/java/com/omc/raffle/domain/exception/RaffleErrorCode.java
@@ -1,13 +1,12 @@
 package com.omc.raffle.domain.exception;
 
-import com.omc.common.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum RaffleErrorCode implements ErrorCode {
+public enum RaffleErrorCode {
 
     RAFFLE_001(HttpStatus.NOT_FOUND, "RAFFLE-001", "해당 래플 이벤트를 찾을 수 없습니다."),
     RAFFLE_002(HttpStatus.BAD_REQUEST, "RAFFLE-002", "이미 래플에 응모하셨습니다."),
