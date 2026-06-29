709a844341817af89a04e164b2d4119b9bc91f1f
kimjaejoong <wcn07003@naver.com>
2026-06-29 15:39:23 +0900

    feat: Kafka 에러 핸들러 및 DLT 구현

---
+++ services/drop-service/src/main/java/com/omc/drop/application/event/KafkaConfig.java
@@ -0,0 +1,21 @@
+package com.omc.drop.application.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public DefaultErrorHandler kafkaErrorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+        DefaultErrorHandler handler = new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3L));
+        handler.addNotRetryableExceptions(JsonProcessingException.class);
+        return handler;
+    }
+}
