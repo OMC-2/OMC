248ca591619f9822736c0781ccb930b96bf9a93c
dahye1111 <chaelee1111@naver.com>
2026-06-30 12:41:52 +0900

    fix: Redis 캐시 타입 정보 직렬화 포맷 수정 (#256)

--- services/product-service/src/main/java/com/omc/product/infrastructure/config/RedisConfig.java
+++ services/product-service/src/main/java/com/omc/product/infrastructure/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.omc.product.infrastructure.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -27,7 +28,8 @@ public class RedisConfig {
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.activateDefaultTyping(
                 objectMapper.getPolymorphicTypeValidator(),
-                ObjectMapper.DefaultTyping.NON_FINAL
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
         );
 
         GenericJackson2JsonRedisSerializer serializer =
